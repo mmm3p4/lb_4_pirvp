7afe0dc8b80288aa7003646840c78df3da5092da
mmm3p4 <100076234+mmm3p4@users.noreply.github.com>
2022-11-10 21:19:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 8081;
+let n = 10;
+let m = 20;
+const fib=(n,m)=> {
+let a = 1;
+let b = 1;
+for (let i = 3; (i <= n); i++) {
+let c = a + b;
+a = b;
+b = c;
+}
+return b;
+
+}
+app.get("/", (req, res) => {
+const a =fib(n,m)
+console.log(a)
+res.send({ success: true, code: fib(n,m)});
+
+
+});
+exports.startServer =function() {
+app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`)
+});
+}
